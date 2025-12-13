5e5d3c558605de7a98ae2776fe34bfcfbcaf37f0
Isaaja <isaiantmaulana2004@gmail.com>
2025-12-13 22:18:25 +0700

    fix bug

--- src/Infrastructures/database/postgres/pool.js
+++ src/Infrastructures/database/postgres/pool.js
@@ -2,10 +2,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === "test"
-      ? process.env.DATABASE_URL_TEST
-      : process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL_TEST || process.env.DATABASE_URL,
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
